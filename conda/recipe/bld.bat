1083ccc0a6f270c6cb505cbefc96ae699697b0a2
mabdazzam <mabdazzam@outlook.com>
2026-04-06 10:14:31 -0600

    add a fixed cross platform recipe which have been tested on windows and linux

--- conda/recipe/bld.bat
+++ conda/recipe/bld.bat
@@ -1,19 +1,20 @@
-@echo off
-setlocal
-
-rem prefer conda host prefix tools/dlls
 set "PATH=%LIBRARY_BIN%;%PATH%"
 
 cmake -S "%SRC_DIR%\src" -B build -G "Ninja" ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%;%PREFIX%" ^
   -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON ^
   -DCMAKE_C_COMPILER="%CC%" ^
-  -DCMAKE_CXX_COMPILER="%CXX%" ^
-  %CMAKE_ARGS% || exit /b 1
+  %CMAKE_ARGS%
+if errorlevel 1 exit /b 1
 
-cmake --build build --parallel %CPU_COUNT% || exit /b 1
-cmake --install build || exit /b 1
+cmake --build build --parallel %CPU_COUNT%
+if errorlevel 1 exit /b 1
+
+cmake --install build
+if errorlevel 1 exit /b 1
+
+if not exist "%LIBRARY_BIN%\gcn10.exe" exit /b 1
 
 endlocal
--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -3,13 +3,14 @@ set -eu
 
 : "${CMAKE_GENERATOR:=Ninja}"
 
-# build from repo root
 cmake -S "${SRC_DIR}/src" -B build -G "${CMAKE_GENERATOR}" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_PREFIX_PATH="${PREFIX}" \
-  -DGCN10_PORTABLE=ON \
+  -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON \
   ${CMAKE_ARGS}
 
 cmake --build build --parallel "${CPU_COUNT}"
 cmake --install build
+
+test -x "${PREFIX}/bin/gcn10"
